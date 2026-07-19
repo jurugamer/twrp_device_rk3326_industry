42907a6fed5b3f52244aa8c28f9a44a29c16744b
jurugamer <73106612+jurugamer@users.noreply.github.com>
2026-07-18 21:41:51 -0300

    BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,13 +10,21 @@ DEVICE_PATH := device/rockchip/rk3326_industry
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
-# Architecture
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
+# Architecture (64-bits nativo para evitar o bug de compatibilidade do Kernel Rockchip)
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := generic
+
+# Suporte secundário a 32-bits
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := generic
 
 TARGET_USES_64_BIT_BINDER := true
 
